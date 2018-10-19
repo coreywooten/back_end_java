6d78a6b040d48e951f1164e8139c6dc4a2a3c077
Corey <ccwooten@aggies.ncat.edu>
2018-10-19 12:46:59 -0400

    Entry Point
    Provides user access to front-end entry point of book lending service

---
+++ main_class.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class main_class {
+
+	public static void main(String[] args) throws IOException {
+		Book_Service service = new Book_Service();
+		service.menu();
+	}
+}
